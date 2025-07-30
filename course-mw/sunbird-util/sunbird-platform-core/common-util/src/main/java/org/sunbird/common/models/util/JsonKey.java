f5d25a4c506685190b994f7cf1f04bfe8ae23d87
sreerag-tarento <sreerag.sajeesh@tarento.com>
2025-07-30 15:33:03 +0530

    Merged code from PR #186

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -1250,7 +1250,6 @@ public final class JsonKey {
   public static final String MULTILINGUAL_COURSE_SWITCH_LOG = "Multilingual course detected. Switching contentId to base language contentId= ";
   public static final String ERROR_MULTILINGUAL_BASE_LANG_NOT_FOUND = "Base language content not found or not live in languageMapV1 for multilingual course.";
   public static final String IS_BASE_LANGUAGE = "isBaseLang";
-  public static final String EXTERNAL_COURSES = "external_courses";
 
   private JsonKey() {}
 }
