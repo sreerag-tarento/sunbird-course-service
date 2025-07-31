e9f2cb4bb2a0ef76aa6eafc2e64b2cb0e482238b
Pathik Nandi <86921116+Pathik-Nandi@users.noreply.github.com>
2025-07-31 10:57:46 +0530

    creating V2 program Enroll API/curatedprogram enroll API which is pointing the New enrollment v2 table. (#185)
    * updated the isBaseLang key
    
    * V2 program Enroll API/curatedprogram enroll API which is pointing the New enrollment v2 table.
    
    * resolved all the comment review and added recent_language to data.
    
    ---------
    
    Co-authored-by: pathiknandi <pathik.nandi@tarento.com>

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/coursebatch/dao/impl/UserCoursesDaoImpl.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/coursebatch/dao/impl/UserCoursesDaoImpl.java
@@ -426,4 +426,29 @@ public class UserCoursesDaoImpl implements UserCoursesDao {
     return getEnrolmentByBatchIdAndCourseIdInternal(requestContext, userId, courseId, batchId, USER_ENROLMENTS_V2);
   }
 
+  public List<UserCourses> extendedReadAllV2(RequestContext requestContext, String userId, String courseId) {
+    Map<String, Object> primaryKey = new HashMap<>();
+    primaryKey.put(JsonKey.USER_ID, userId);
+    primaryKey.put(JsonKey.COURSE_ID, courseId);
+    Response response = cassandraOperation.getRecordByIdentifier(requestContext, KEYSPACE_NAME, USER_ENROLMENTS_V2, primaryKey, null);
+    List<Map<String, Object>> userCoursesList =
+            (List<Map<String, Object>>) response.get(JsonKey.RESPONSE);
+    if (CollectionUtils.isEmpty(userCoursesList)) {
+      return null;
+    }
+    try {
+      List<UserCourses> convertedUserCoursesList = new ArrayList<>();
+      for (Object userCourseObj : userCoursesList) {
+        if (userCourseObj instanceof Map) {
+          Map<String, Object> userCourseMap = (Map<String, Object>) userCourseObj;
+          convertedUserCoursesList.add(mapper.convertValue(userCourseMap, UserCourses.class));
+        }
+      }
+      return convertedUserCoursesList;
+    } catch (Exception e) {
+      logger.error(requestContext, "Failed to read user enrollments table. Exception: ", e);
+    }
+    return null;
+  }
+
 }
