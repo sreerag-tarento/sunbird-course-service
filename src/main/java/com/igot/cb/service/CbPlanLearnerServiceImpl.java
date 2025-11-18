45f8bcf627ccad54376b5c2ad7e69e24061cf98a
Arpitha <arpitha.sureshappa@tarento.com>
2025-11-18 16:20:37 +0530

    11900 apar plan list issue fix (#149)
    * KB-11900 apar duplicate courselist issue fix
    
    * 11900 apar plan list issue fix
    
    * 11900 apar plan list issue fix

--- src/main/java/com/igot/cb/service/CbPlanLearnerServiceImpl.java
+++ src/main/java/com/igot/cb/service/CbPlanLearnerServiceImpl.java
@@ -274,39 +274,36 @@ public class CbPlanLearnerServiceImpl {
         for (String courseId : courses) {
             Map<String, Object> contentDetails = null;
 
-            if (!courseDetailsMap.containsKey(courseId)) {
-                contentDetails = contentService.readContent(courseId, null);
 
-                if (MapUtils.isNotEmpty(contentDetails)) {
-                    if (courseId.contains("_rc")) {
-                        if (Constants.VERIFIED.equalsIgnoreCase(userProfile.get(Constants.PROFILE_STATUS_KEY))) {
-                            Object secureSettingsObj = contentDetails.get(Constants.SECURE_SETTINGS);
-                            if (secureSettingsObj instanceof Map<?, ?> secureSettings && !secureSettings.isEmpty()) {
-                                Object orgListObj = secureSettings.get(Constants.ORGANISATION);
-                                if (orgListObj instanceof List<?> orgList && !orgList.isEmpty()) {
-                                    List<String> secureOrgList = orgList.stream()
-                                            .filter(String.class::isInstance)
-                                            .map(String.class::cast)
-                                            .toList();
+            contentDetails = contentService.readContent(courseId, null);
 
-                                    if (secureOrgList.contains(userOrgId)) {
-                                        courseDetailsMap.put(courseId, contentDetails);
-                                    }
+            if (MapUtils.isNotEmpty(contentDetails)) {
+                if (courseId.contains("_rc")) {
+                    if (Constants.VERIFIED.equalsIgnoreCase(userProfile.get(Constants.PROFILE_STATUS_KEY))) {
+                        Object secureSettingsObj = contentDetails.get(Constants.SECURE_SETTINGS);
+                        if (secureSettingsObj instanceof Map<?, ?> secureSettings && !secureSettings.isEmpty()) {
+                            Object orgListObj = secureSettings.get(Constants.ORGANISATION);
+                            if (orgListObj instanceof List<?> orgList && !orgList.isEmpty()) {
+                                List<String> secureOrgList = orgList.stream()
+                                        .filter(String.class::isInstance)
+                                        .map(String.class::cast)
+                                        .toList();
+
+                                if (secureOrgList.contains(userOrgId)) {
+                                    courseDetailsMap.put(courseId, contentDetails);
                                 }
                             }
                         }
+                    }
 
-                        if (!courseDetailsMap.containsKey(courseId)) {
-                            contentDetails.clear();
-                        }
-                    } else {
-                        courseDetailsMap.put(courseId, contentDetails);
+                    if (!courseDetailsMap.containsKey(courseId)) {
+                        contentDetails.clear();
                     }
                 } else {
-                    logger.error("Failed to read course details for Id: {}", courseId);
+                    courseDetailsMap.put(courseId, contentDetails);
                 }
             } else {
-                continue;
+                logger.error("Failed to read course details for Id: {}", courseId);
             }
 
             if (MapUtils.isNotEmpty(contentDetails)) {
